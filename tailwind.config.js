17723b1bb7e54aec5b49b16671e2cf66aa44035d
tuanfront_end <atuanvptd@gmail.com>
2021-02-16 18:40:25 +0700

    update init

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        DEFAULT: "10px",
+        DEFAULT: "16px",
         "2xl": "128px",
       },
     },
@@ -41,25 +41,6 @@ module.exports = {
       secondary: "#B3421A",
       black: "#121212",
       white: "fff",
-      gray: {
-        100: "#1C1C1C",
-        200: "#414141",
-        300: "#848484",
-        400: "#9B9B9B",
-        500: "#AEAEAE",
-        600: "#C0C0C0",
-        700: "#D1D1D1",
-        800: "#E0E0E0",
-        900: "#EEEEEE",
-      },
-      accent: {
-        1: "#ECB2F2",
-        2: "#2D6A6A",
-        3: "#E9AD8C",
-        4: "#221874",
-        5: "#7CC6D6",
-        6: "#E1604D",
-      },
       status: {
         success: "#287D3C",
         warning: "#B95000",
@@ -77,9 +58,6 @@ module.exports = {
       twitter: "#00AADB",
     },
     extend: {
-      maxWidth: {
-        body: "680px",
-      },
       outline: {},
       borderColor: {
         primary: "#FFDCBE",
@@ -89,29 +67,7 @@ module.exports = {
         black: "#000",
         white: "#fff",
       },
-      boxShadow: {
-        tight: {
-          1: "4px 4px 0px #F1876F, 8px 8px 0px #F5AE9E",
-          2: "0px 1.25px 4px rgba(0, 0, 0, 0.25)",
-          3: "0px 2px 6px rgba(0, 0, 0, 0.22)",
-          4: "0px 2px 7px rgba(0, 0, 0, 0.24)",
-          5: "0px 3px 12px rgba(0, 0, 0, 0.2)",
-          6: "0px 3.5px 14px rgba(0, 0, 0, 0.2)",
-          7: "0px 7px 18px rgba(0, 0, 0, 0.17)",
-          8: "0px 12px 28px rgba(0, 0, 0, 0.2)",
-          9: "0px 13px 36px rgba(0, 0, 0, 0.22)",
-        },
-        fluffy: {
-          1: "0px 5px 15px rgba(0, 0, 0, 0.15)",
-          2: "0px 6.25px 20px rgba(0, 0, 0, 0.125)",
-          3: "0px 10px 30px rgba(0, 0, 0, 0.11)",
-          4: "0px 10px 35px rgba(0, 0, 0, 0.12)",
-          5: "0px 15px 60px rgba(0, 0, 0, 0.12)",
-          6: "0px 17.5px 70px rgba(0, 0, 0, 0.12)",
-          7: "0px 35px 90px rgba(0, 0, 0, 0.135)",
-        },
-        button: "4px 4px 0px #F1876F, 8px 8px 0px #F5AE9E",
-      },
+      boxShadow: {},
       zIndex: {
         "-1": "-1",
         max: 2147483647,
@@ -139,9 +95,6 @@ module.exports = {
         "3px": "3px",
       },
       fontSize: {
-        "d-lare": ["107px", "97.2%"],
-        "d-small": ["86px", "102.33%"],
-        //
         f1: ["55px", "101.82%"],
         f2: ["44px", "109.09%"],
         f3: ["35px", "114.29%"],
@@ -149,24 +102,6 @@ module.exports = {
         f5: ["22.5px", "124.44%"],
         f6: ["18px", "155.56%"],
         //
-        "paragraph-large": ["28px", { lineHeight: "128.57%" }],
-        "paragraph-base": [
-          "18px",
-          {
-            letterSpacing: "1rem",
-            lineHeight: "155.56%",
-          },
-        ],
-        "paragraph-small": ["16px", { lineHeight: "150%" }],
-        "paragraph-xs-small": ["14px", { lineHeight: "142.86%" }],
-        "paragraph-tiny": ["12px", { lineHeight: "133.33%" }],
-        "small-caps": [
-          "18px",
-          {
-            lineHeight: "111.11%",
-            letterSpacing: "5%",
-          },
-        ],
       },
     },
   },
